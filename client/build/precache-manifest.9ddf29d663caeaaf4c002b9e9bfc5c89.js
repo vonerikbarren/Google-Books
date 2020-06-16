39a10c8d04ace20767f553af7b990aed1d1fdac0
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2020-06-16 06:54:33 -0400

    initial commit of google search

---
+++ client/build/precache-manifest.9ddf29d663caeaaf4c002b9e9bfc5c89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88d45724c233508852a8",
+    "url": "/static/css/main.ffcef0c9.chunk.css"
+  },
+  {
+    "revision": "88d45724c233508852a8",
+    "url": "/static/js/main.4f9ff331.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f14e103a85ded4a2db8",
+    "url": "/static/js/2.9f08725b.chunk.js"
+  },
+  {
+    "revision": "74faa3f11946b66788457dfec8f8c643",
+    "url": "/index.html"
+  }
+];
